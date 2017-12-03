ca7c9df41345eab8922400678f187061156291aa
ermangurses <egurses@email.arizona.edu>
2017-12-02 19:51:51 -0700

    Maven configurations - adding dependencies

--- SemanticTextEditor/src/edu/arizona/biosemantics/client/GreetingService.java
+++ SemanticTextEditor/src/edu/arizona/biosemantics/client/GreetingService.java
@@ -3,6 +3,8 @@ package edu.arizona.biosemantics.client;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
+
+
 /**
  * The client-side stub for the RPC service.
  */
