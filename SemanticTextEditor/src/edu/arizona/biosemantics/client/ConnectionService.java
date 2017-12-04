c090bc0040594be02b617862a47106d40c8ea467
ermangurses <egurses@email.arizona.edu>
2017-12-03 23:48:44 -0700

    The path name was changed in XML file

--- SemanticTextEditor/src/edu/arizona/biosemantics/client/ConnectionService.java
+++ SemanticTextEditor/src/edu/arizona/biosemantics/client/ConnectionService.java
@@ -8,7 +8,7 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 /**
  * The client-side stub for the RPC service.
  */
-@RemoteServiceRelativePath("greet")
+@RemoteServiceRelativePath("connect")
 public interface ConnectionService extends RemoteService {
 	String greetServer(String name) throws IllegalArgumentException;
 }
