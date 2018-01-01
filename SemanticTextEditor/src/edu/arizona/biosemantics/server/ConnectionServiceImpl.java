6bc4cec36b6347fcfa8b7967330d8e6bdf02d997
ermangurses <egurses@email.arizona.edu>
2018-01-01 11:18:57 -0700

    Basic formatting

--- SemanticTextEditor/src/edu/arizona/biosemantics/server/ConnectionServiceImpl.java
+++ SemanticTextEditor/src/edu/arizona/biosemantics/server/ConnectionServiceImpl.java
@@ -15,50 +15,53 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
  */
 @SuppressWarnings("serial")
 public class ConnectionServiceImpl extends RemoteServiceServlet implements ConnectionService {
-  	
-	OWLAccessorImpl accessor = new OWLAccessorImpl("https://raw.githubusercontent.com/pato-ontology/pato/master/pato.owl");
-	public List<String> sendWord(String token) throws IllegalArgumentException {
-	    
-	    if(accessor.isClassLabel(token)) {
-	        return accessor.getExactSynonymsfromMap(token);    	        
-	    }else {
-	        List<String> list =  Arrays.asList("There is no Synonym"); 
-	        return list;       
-	    }	    
-	}
-	public boolean callMappingLabelsToExactSynonyms() throws IllegalArgumentException {
+
+    OWLAccessorImpl accessor = new OWLAccessorImpl("https://raw.githubusercontent.com/pato-ontology/pato/master/pato.owl");
+	
+    public List<String> sendWord(String token) throws IllegalArgumentException {
+        
+        if(accessor.isClassLabel(token)) {
+            return accessor.getExactSynonymsfromMap(token);    	        
+	       } else {
+	           List<String> list =  Arrays.asList("There is no Synonym"); 
+	           return list;       
+	       }	    
+	   }
+    
+    public boolean callMappingLabelsToExactSynonyms() throws IllegalArgumentException {
 		
-		if(accessor.mapLabelsToExactSynonyms()){
-		    System.out.println("Loading of map is successful!");
-		    return true;
-		}else {
+        if(accessor.mapLabelsToExactSynonyms()){
+		          System.out.println("Loading of map is successful!");
+		          return true;
+        } else {
             System.out.println("Loading of map is failed!");	
             return false;
-		}
-	}
+		      }
+    }
 	
-	@Override
-	public List<String> sendSentence(String list) {
-		ArrayList<String> tokenizedTextFromTextArea = tokenizeSentence(list);
-	    for (int counter = 0; counter < tokenizedTextFromTextArea.size(); counter++) { 	
-	          System.out.println(tokenizedTextFromTextArea.get(counter)); 
-	    }
-		return tokenizedTextFromTextArea;	
-	}
-
-	private ArrayList<String> tokenizeSentence(String sentence) {
-		String token = null;
-	    StringTokenizer stringTokenizer = 
-	             new StringTokenizer(sentence,",  .");  
-	     ArrayList<String> tokens = new ArrayList<String>();
-	      
-	      while (stringTokenizer.hasMoreElements()) {
-	    	    token = stringTokenizer.nextElement().toString();
-	        //System.out.println(token);
-	        tokens.add(token);            
-	      }
-	    return tokens;
-	 }
+    @Override
+    public List<String> sendSentence(String list) {
 		
-	
+        ArrayList<String> tokenizedTextFromTextArea = tokenizeSentence(list);
+	    
+        for (int counter = 0; counter < tokenizedTextFromTextArea.size(); counter++) { 	
+            System.out.println(tokenizedTextFromTextArea.get(counter)); 
+	       }
+        return tokenizedTextFromTextArea;
+    }
+
+    private ArrayList<String> tokenizeSentence(String sentence) {
+		
+        String token = null;
+        StringTokenizer stringTokenizer = new StringTokenizer(sentence,",  .");  
+	       ArrayList<String> tokens = new ArrayList<String>();
+	       
+	       while (stringTokenizer.hasMoreElements()) {
+	           
+	           token = stringTokenizer.nextElement().toString();
+	           //System.out.println(token);
+	           tokens.add(token);            
+	       }
+	       return tokens;
+    }
 }
