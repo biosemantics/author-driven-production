845c446000eb2151d0efafccc705e12a95fd4ec9
ermangurses <egurses@email.arizona.edu>
2018-01-01 11:19:46 -0700

    Basic formatting

--- SemanticTextEditor/src/edu/arizona/biosemantics/client/ConnectionService.java
+++ SemanticTextEditor/src/edu/arizona/biosemantics/client/ConnectionService.java
@@ -4,15 +4,12 @@ import java.util.List;
 
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
-
-
-
 /**
  * The client-side stub for the RPC service.
  */
 @RemoteServiceRelativePath("connect")
 public interface ConnectionService extends RemoteService {
-	List<String> sendSentence(String sentence);
-	List<String> sendWord(String token);
+    List<String> sendSentence(String sentence);
+    List<String> sendWord(String token);
          boolean callMappingLabelsToExactSynonyms();
 }
